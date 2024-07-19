d17c69133aff12ea3141b3f0b6840247c9267f85
devanshu <devanshusenp@gmail.com>
2024-07-19 02:47:45 -0500

    added litellm

---
+++ bestintern/api/llm/llm.py
@@ -0,0 +1,30 @@
+"""Talk to LLMs"""
+
+from litellm import completion
+
+from config.config import LiteLLMModels, LiteLLMResponse
+
+
+class LiteLLM:
+    def __init__(
+        self, model: LiteLLMModels, system_prompt: str = "", num_retries: int = 0
+    ) -> None:
+        self.model = model
+        self.system_prompt = system_prompt
+        self.num_retries = num_retries
+
+    def askllm(
+        self, query: str, overwrite_system_prompt: str = None
+    ) -> LiteLLMResponse:
+        messages = []
+
+        system_prompt = overwrite_system_prompt or self.system_prompt
+        messages.append({"role": "system", "content": system_prompt})
+        messages.append({"role": "user", "content": query})
+
+        response = completion(
+            model=self.model, messages=messages, num_retries=self.num_retries
+        )
+
+        llmresponse = LiteLLMResponse(content=response.choices[0].message.content)
+        return llmresponse
