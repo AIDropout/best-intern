2d8a81219584c0f0563639ab2c6feb45b2cf20c3
devanshu <devanshusenp@gmail.com>
2024-07-19 04:48:06 -0500

    organized intro parse services folder

--- bestintern/services/parse/job.py
+++ bestintern/services/parse/job.py
@@ -1,4 +1,8 @@
 # parse through job links to gather data and build job models
 # uses both the web reader and the llm
 
+# 1. get a link
+# 2. determine a way to get the data
+# 3. ask the llm to parse through the data
+
 # output/return a job model (for now preferrably to txt in a folder)
--- bestintern/services/parse/resume.py
+++ bestintern/services/parse/resume.py
@@ -1,4 +1,8 @@
 # parse through resumes to build a model
 # this will use both the pdf reader and the llm
 
+# 1. get a resume
+# 2. extract data and metadata
+# 3. ask the llm to parse through the data
+
 # output/return a resume model (for now preferrably to txt in a folder)
