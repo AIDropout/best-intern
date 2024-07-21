c7794512e3396938fd69c6396937f95cc947ac52
devanshu <devanshusenp@gmail.com>
2024-07-21 02:35:49 -0500

    weave tracking BABY

--- bestintern/tools/llm/llm.py
+++ bestintern/tools/llm/llm.py
@@ -2,12 +2,14 @@
 
 from enum import Enum
 
+import weave
 from litellm import completion
 from pydantic import BaseModel
 
 
 class LiteLLMModels(Enum):
     gemini = "gemini/gemini-pro"  # requires GEMINI_API_KEY
+    gemini_flash = "gemini/gemini-1.5-flash"  # requires GEMINI_API_KEY
 
 
 class LiteLLMConfig(BaseModel):
@@ -19,16 +21,20 @@ class LiteLLMResponse(BaseModel):
 
 
 class LiteLLM:
+    weave.init("bestintern")
+
     def __init__(
         self, model: LiteLLMModels, system_prompt: str = "", num_retries: int = 0
     ) -> None:
-        self.model = model
+        self.model = model.value
         self.system_prompt = system_prompt
         self.num_retries = num_retries
 
+    @weave.op()
     def askllm(
         self, query: str, overwrite_system_prompt: str = None
     ) -> LiteLLMResponse:
+
         messages = []
 
         system_prompt = overwrite_system_prompt or self.system_prompt
